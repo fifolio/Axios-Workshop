67a4a0ff06bca4b179c5ad7ae8b2ac0368155b0f
Firas <firasdabbabi@gmail.com>
2023-03-13 11:07:09 +0100

    version 1.1: Get & Post, methods

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 // Import Axios
 const axios = require("axios");
 
+// ----------------[POST]-----------------
+const userId = 1996,
+  title = "My Best Post",
+  body = "This is something I really love to do";
+// Call Axios to fetch data
+axios({
+  url: "/posts", // Slug
+  baseURL: "https://jsonplaceholder.typicode.com", // Base
+  method: "post", // Fetch Type
+  data: {
+    userId: userId,
+    title: title,
+    body: body,
+  },
+})
+  .then((res) => console.log(res.data))
+  .catch((err) => console.log(err));
+
+// ----------------[GET]-----------------
 // Call Axios to fetch data
 axios({
   url: "/todos", // Slug
   baseURL: "https://jsonplaceholder.typicode.com", // Base
   method: "get", // Fetch Type
-})
-  .then((res) => {
-    console.log(res.data[1].title);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+});
+//   .then((res) => {
+//     console.log(res.data[1].title);
+//     console.log(res.status);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
